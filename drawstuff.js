12b02f8679a54493f4dfc8da7c2482e02abb21af
Xingyao Huang <xhuang17@ncsu.edu>
2017-09-20 10:50:28 -0400

    version 2

--- drawstuff.js
+++ drawstuff.js
@@ -38,8 +38,239 @@ class Color {
             console.log(e);
         }
     } // end Color change method
+
+        // Color add method
+    add(c) {
+        try {
+            if (!(c instanceof Color))
+                throw "Color.add: non-color parameter";
+            else {
+                this.r += c.r; this.g += c.g; this.b += c.b; this.a += c.a;
+                return(this);
+            }
+        } // end try
+        
+        catch(e) {
+            console.log(e);
+        }
+    } // end color add
+    
+        // Color subtract method
+    subtract(c) {
+        try {
+            if (!(c instanceof Color))
+                throw "Color.subtract: non-color parameter";
+            else {
+                this.r -= c.r; this.g -= c.g; this.b -= c.b; this.a -= c.a;
+                return(this);
+            }
+        } // end try
+        
+        catch(e) {
+            console.log(e);
+        }
+    } // end color subgtract
+    
+        // Color scale method
+    scale(s) {
+        try {
+            if (typeof(s) !== "number")
+                throw "scale factor not a number";
+            else {
+                this.r *= s; this.g *= s; this.b *= s; this.a *= s; 
+                return(this);
+            }
+        } // end throw
+        
+        catch (e) {
+            console.log(e);
+        }
+    } // end Color scale method
+    
+        // Color copy method
+    copy(c) {
+        try {
+            if (!(c instanceof Color))
+                throw "Color.copy: non-color parameter";
+            else {
+                this.r = c.r; this.g = c.g; this.b = c.b; this.a = c.a;
+                return(this);
+            }
+        } // end try
+        
+        catch(e) {
+            console.log(e);
+        }
+    } // end Color copy method
+    
+        // Color clone method
+    clone() {
+        var newColor = new Color();
+        newColor.copy(this);
+        return(newColor);
+    } // end Color clone method
+    
+        // Send color to console
+    toConsole() {
+        console.log("rgba: "+ this.r +" "+ this.g +" "+ this.b +" "+ this.a);
+    }  // end Color toConsole
+    
 } // end color class
 
+// Vector class
+class Vector { 
+    constructor(x,y,z) {
+        this.set(x,y,z);
+    } // end constructor
+    
+    // sets the components of a vector
+    set(x,y,z) {
+        try {
+            if ((typeof(x) !== "number") || (typeof(y) !== "number") || (typeof(z) !== "number"))
+                throw "vector component not a number";
+            else
+                this.x = x; this.y = y; this.z = z; 
+        } // end try
+        
+        catch(e) {
+            console.log(e);
+        }
+    } // end vector set
+    
+    // copy the passed vector into this one
+    copy(v) {
+        try {
+            if (!(v instanceof Vector))
+                throw "Vector.copy: non-vector parameter";
+            else
+                this.x = v.x; this.y = v.y; this.z = v.z;
+        } // end try
+        
+        catch(e) {
+            console.log(e);
+        }
+    }
+    
+    toConsole(prefix="") {
+        console.log(prefix+"["+this.x+","+this.y+","+this.z+"]");
+    } // end to console
+    
+    // static dot method
+    static dot(v1,v2) {
+        try {
+            if (!(v1 instanceof Vector) || !(v2 instanceof Vector))
+                throw "Vector.dot: non-vector parameter";
+            else
+                return(v1.x*v2.x + v1.y*v2.y + v1.z*v2.z);
+        } // end try
+        
+        catch(e) {
+            console.log(e);
+            return(NaN);
+        }
+    } // end dot static method
+    
+    // static cross method
+    static cross(v1,v2) {
+        try {
+            if (!(v1 instanceof Vector) || !(v2 instanceof Vector))
+                throw "Vector.cross: non-vector parameter";
+            else {
+                var crossX = v1.y*v2.z - v1.z*v2.y;
+                var crossY = v1.z*v2.x - v1.x*v2.z;
+                var crossZ = v1.x*v2.y - v1.y*v2.x;
+                return(new Vector(crossX,crossY,crossZ));
+            } // endif vector params
+        } // end try
+        
+        catch(e) {
+            console.log(e);
+            return(NaN);
+        }
+    } // end dot static method
+    
+    // static add method
+    static add(v1,v2) {
+        try {
+            if (!(v1 instanceof Vector) || !(v2 instanceof Vector))
+                throw "Vector.add: non-vector parameter";
+            else
+                return(new Vector(v1.x+v2.x,v1.y+v2.y,v1.z+v2.z));
+        } // end try
+        
+        catch(e) {
+            console.log(e);
+            return(new Vector(NaN,NaN,NaN));
+        }
+    } // end add static method
+
+    // static subtract method, v1-v2
+    static subtract(v1,v2) {
+        try {
+            if (!(v1 instanceof Vector) || !(v2 instanceof Vector))
+                throw "Vector.subtract: non-vector parameter";
+            else {
+                var v = new Vector(v1.x-v2.x,v1.y-v2.y,v1.z-v2.z);
+                return(v);
+            }
+        } // end try
+        
+        catch(e) {
+            console.log(e);
+            return(new Vector(NaN,NaN,NaN));
+        }
+    } // end subtract static method
+
+    // static scale method
+    static scale(c,v) {
+        try {
+            if (!(typeof(c) === "number") || !(v instanceof Vector))
+                throw "Vector.scale: malformed parameter";
+            else
+                return(new Vector(c*v.x,c*v.y,c*v.z));
+        } // end try
+        
+        catch(e) {
+            console.log(e);
+            return(new Vector(NaN,NaN,NaN));
+        }
+    } // end scale static method
+
+
+    // static scale method
+    static scale2(c,v) {
+        try {
+            if (!(c instanceof Vector) || !(v instanceof Vector))
+                throw "Vector.scale: malformed parameter";
+            else
+                return(new Vector(c.x*v.x,c.y*v.y,c.z*v.z));
+        } // end try
+        
+        catch(e) {
+            console.log(e);
+            return(new Vector(NaN,NaN,NaN));
+        }
+    } // end scale static method
+    
+    // static normalize method
+    static normalize(v) {
+        try {
+            if (!(v instanceof Vector))
+                throw "Vector.normalize: parameter not a vector";
+            else {
+                var lenDenom = 1/Math.sqrt(Vector.dot(v,v));
+                return(Vector.scale(lenDenom,v));
+            }
+        } // end try
+        
+        catch(e) {
+            console.log(e);
+            console.log(v);
+            return(new Vector(NaN,NaN,NaN));
+        }
+    } // end scale static method
+    
+} // end Vector class
 
 /* utility functions */
 
@@ -62,6 +293,7 @@ function drawPixel(imagedata,x,y,color) {
     
     catch(e) {
         console.log(e);
+        console.log("x: " + x + "  y: " + y);
     }
 } // end drawPixel
     
@@ -86,6 +318,22 @@ function drawRandPixels(context) {
     context.putImageData(imagedata, 0, 0);
 } // end draw random pixels
 
+
+function getEllipsoidZ(x0, y0, ellipsoid) {
+    var a = ellipsoid.a;
+    var b = ellipsoid.b;
+    var c = ellipsoid.c;
+    var x = ellipsoid.x;
+    var y = ellipsoid.y;
+    var z = ellipsoid.z;
+    var root = 1 - Math.pow(x0-x, 2) / Math.pow(a, 2) - Math.pow(y0-y, 2) / Math.pow(b, 2);
+    if(root < 0) {
+        return -1000;
+    }
+    var result = c * Math.sqrt(root) + z;
+    return result;
+}
+
 // get the input ellipsoids from the standard class URL
 function getInputEllipsoids() {
     const INPUT_ELLIPSOIDS_URL = 
@@ -94,6 +342,27 @@ function getInputEllipsoids() {
     // load the ellipsoids file
     var httpReq = new XMLHttpRequest(); // a new http request
     httpReq.open("GET",INPUT_ELLIPSOIDS_URL,false); // init the request
+    httpReq.send(null); // send the request
+    var startTime = Date.now();
+    while ((httpReq.status !== 200) && (httpReq.readyState !== XMLHttpRequest.DONE)) {
+        if ((Date.now()-startTime) > 3000)
+            break;
+    } // until its loaded or we time out after three seconds
+    if ((httpReq.status !== 200) || (httpReq.readyState !== XMLHttpRequest.DONE)) {
+        console.log*("Unable to open input ellipses file!");
+        return String.null;
+    } else
+        return JSON.parse(httpReq.response); 
+} // end get input ellipsoids
+
+// get the input lights from the standard class URL
+function getInputLights() {
+    const INPUT_LIGHTS_URL = 
+        "https://ncsucgclass.github.io/prog1/lights.json";
+        
+    // load the lights file
+    var httpReq = new XMLHttpRequest(); // a new http request
+    httpReq.open("GET",INPUT_LIGHTS_URL,false); // init the request
     httpReq.send(null); // send the request
     var startTime = Date.now();
     while ((httpReq.status !== 200) && (httpReq.readyState !== XMLHttpRequest.DONE)) {
@@ -195,6 +464,121 @@ function drawInputEllipsoidsUsingArcs(context) {
 } // end draw input ellipsoids
 
 
+// draw 2d projections read from the JSON file at class github
+function drawInputEllipsoidsUsingRayCasting(context) {
+    var inputEllipsoids = getInputEllipsoids();
+    var inputLights = getInputLights();
+    var eye = new Vector(0.5, 0.5, -5);
+    
+    console.log(inputEllipsoids);
+    console.log(inputLights);
+
+
+    if (inputEllipsoids != String.null) {
+        var c = new Color(0,0,0,0); // the color at the pixel: black
+
+        var width = context.canvas.width;
+        var height = context.canvas.height;
+
+        var nl = inputEllipsoids.length;
+        var ll = inputLights.length;
+
+        var imagedata = context.createImageData(width, height);
+        
+        for(var i = 0; i < 1; i += 1/width) {
+            for(var j = 1/height; j <= 1; j += 1/height) {
+                
+                // find display object
+                var displayObject = nl;
+                
+                var min_z = -999;
+                for(var k = 0; k < nl; k++) {
+                    var z = getEllipsoidZ(i, j, inputEllipsoids[k]);
+                    if(z > min_z) {
+                        min_z = z;
+                        displayObject = k;
+
+                    }
+                }
+
+
+                // // display: part 1
+                // if(displayObject < nl) {
+                //     var fillColor = new Color(inputEllipsoids[displayObject].diffuse[0]*255,
+                //                             inputEllipsoids[displayObject].diffuse[1]*255,
+                //                             inputEllipsoids[displayObject].diffuse[2]*255,
+                //                             255);
+                //     drawPixel(imagedata, i*width, (1-j)*height, fillColor);
+                // }
+                // else {
+                //     drawPixel(imagedata, i*width, (1-j)*height, new Color(0,0,0,255));
+                // }
+
+                // display: part 2
+                if(displayObject < nl) {
+                    var objectPos = new Vector(i, j, min_z);
+                    var normal = Vector.scale2(
+                                        Vector.subtract(
+                                                new Vector(inputEllipsoids[displayObject].x,
+                                                        inputEllipsoids[displayObject].y,
+                                                        inputEllipsoids[displayObject].z
+                                                        ),
+                                                objectPos 
+                                                ),
+                                        new Vector(
+                                                2/Math.pow(inputEllipsoids[displayObject].a, 2),
+                                                2/Math.pow(inputEllipsoids[displayObject].b, 2),
+                                                2/Math.pow(inputEllipsoids[displayObject].c, 2)
+                                            )
+                                        );
+
+                    var fillColor = new Color(0, 0, 0, 1);
+
+                    // ambient
+                    var ambient = new Color(inputEllipsoids[displayObject].ambient[0],
+                                            inputEllipsoids[displayObject].ambient[1],
+                                            inputEllipsoids[displayObject].ambient[2],
+                                            1);
+                    fillColor = fillColor.add(ambient);
+
+                    // illum for all lights
+                    for(var light = 0; light < ll; light++) {
+                        // diffuse
+                        var ligth_vector = new Vector(1-inputLights[light].x, 1-inputLights[light].y, inputLights[light].z);
+                        var n = Vector.normalize(normal);
+                        var l = Vector.normalize(Vector.subtract(ligth_vector, objectPos));
+                        var NdotL = Math.max(0,Vector.dot(n, l));
+                        var diffuse = new Color(inputEllipsoids[displayObject].diffuse[0] * inputLights[light].diffuse[0] * NdotL,
+                                                inputEllipsoids[displayObject].diffuse[1] * inputLights[light].diffuse[1] * NdotL,
+                                                inputEllipsoids[displayObject].diffuse[2] * inputLights[light].diffuse[2] * NdotL,
+                                                1);
+                        fillColor = fillColor.add(diffuse);
+
+                        // specular
+                        var v = Vector.normalize(Vector.subtract(eye, objectPos));
+                        // var v = new Vector(0, 0, -1);
+                        var h = Vector.normalize(Vector.add(v,l));
+                        var q = inputEllipsoids[displayObject].n;
+                        var NdotH = Math.pow(Math.max(0,Vector.dot(n, h)),q);
+                        var specular = new Color(inputEllipsoids[displayObject].specular[0] * inputLights[light].specular[0] * NdotH,
+                                                inputEllipsoids[displayObject].specular[1] * inputLights[light].specular[1] * NdotH,
+                                                inputEllipsoids[displayObject].specular[2] * inputLights[light].specular[2] * NdotH,
+                                                1);
+                        fillColor = fillColor.add(specular);
+                    }
+                    fillColor = fillColor.scale(255);
+                    drawPixel(imagedata, i*width, (1-j)*height, fillColor);
+                }
+                else {
+                    // Background
+                    drawPixel(imagedata, i*width, (1-j)*height, new Color(0,0,0,255));
+                }
+            }
+        }
+        context.putImageData(imagedata, 0, 0);
+    } // end if ellipsoids found
+} // end draw input ellipsoids
+
 /* main -- here is where execution begins after window load */
 
 function main() {
@@ -204,12 +588,14 @@ function main() {
     var context = canvas.getContext("2d");
  
     // Create the image
-    //drawRandPixels(context);
+    // drawRandPixels(context);
       // shows how to draw pixels
     
-    //drawRandPixelsInInputEllipsoids(context);
+    // drawRandPixelsInInputEllipsoids(context);
       // shows how to draw pixels and read input file
       
-    drawInputEllipsoidsUsingArcs(context);
+    // drawInputEllipsoidsUsingArcs(context);
       // shows how to read input file, but not how to draw pixels
+
+    drawInputEllipsoidsUsingRayCasting(context);
 }
